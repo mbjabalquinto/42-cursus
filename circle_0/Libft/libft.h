645e8b997cc290d2fa06d6e1ce999d6208a6db8c
mbjabalquinto <124250038+mbjabalquinto@users.noreply.github.com>
2025-11-08 15:11:44 +0100

    Create libft.h

---
+++ circle_0/Libft/libft.h
@@ -0,0 +1,11 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+
+void	ft_putchar(char c);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+// ... y así sucesivamente para todas tus funciones.
+
+#endif
