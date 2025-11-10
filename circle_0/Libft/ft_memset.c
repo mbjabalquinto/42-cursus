98c8f08d6f93fca76ce5463b45ce800b07c85cc8
mbjabalquinto <124250038+mbjabalquinto@users.noreply.github.com>
2025-11-10 12:08:36 +0100

    Add files via upload

---
+++ circle_0/Libft/ft_memset.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjabalqu <mjabalqu@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 13:09:22 by mjabalqu          #+#    #+#             */
+/*   Updated: 2025/11/06 15:37:38 by mjabalqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*memset(void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*p;
+
+	p = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		p[i] = (unsigned char)c;
+		i++;
+	}
+	return (s);
+}
