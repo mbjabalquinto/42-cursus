2014f76bcf53c124907d8ccdeb6b96cd5c5cfe82
Marcos Jabalquinto Ibanez <mjabalqu@c2r2s3.42malaga.com>
2025-11-17 15:00:04 +0100

    update split

--- circle_0/Libft/ft_split.c
+++ circle_0/Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mjabalqu <mjabalqu@student.42malaga.com>  +#+  +:+       +#+         */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 17:50:47 by mjabalqu         #+#    #+#              */
-/*   Updated: 2025/11/17 13:54:15 by mjabalqu         ###   ########.fr       */
+/*   Updated: 2025/11/17 14:19:39 by mjabalqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,10 +14,10 @@
 int	ft_count_words(char const *s, char const *init_pos, char c)
 {
 	int	cont;
-	
+
 	cont = 0;
 	while (*s != '\0')
-	{		
+	{
 		if (*s != c && s == init_pos)
 			cont++;
 		else if (*s == c && *(s + 1) != c && *s != '\0')
@@ -27,13 +27,12 @@ int	ft_count_words(char const *s, char const *init_pos, char c)
 	return (cont);
 }
 
-void	ft_first_word(char **end_word, char **start_word, char const *s, char c, char ***words)
+void	ft_first_word(char **end_word, char c, char const *s, char ***words)
 {
 	*end_word = ft_strchr(s, (int)c);
 	if (*end_word)
-	{	
-		*start_word = (char *)s;
-		**words = ft_substr(s, 0, (size_t)(*end_word - *start_word));
+	{
+		**words = ft_substr(s, 0, (size_t)(*end_word - (char *)s));
 		(*words)++;
 	}
 }
@@ -41,7 +40,7 @@ void	ft_first_word(char **end_word, char **start_word, char const *s, char c, ch
 void	ft_last_word(char *last_pos, char ***words, char c)
 {
 	size_t	len;
-	
+
 	while (*last_pos == c)
 		last_pos++;
 	len = ft_strlen(last_pos);
@@ -49,22 +48,23 @@ void	ft_last_word(char *last_pos, char ***words, char c)
 	(*words)++;
 }
 
-void	ft_separate_words(char const *init_pos, char const *s, char **words, char c)
-{	
+void	ft_separate_words(char const *init_pos, char const *s
+		, char **words, char c)
+{
 	char	*start_word;
 	char	*end_word;
 	char	*last_pos;
-	
+
 	while (*s != '\0')
-	{		
+	{
 		if (*s != c && s == init_pos)
-			ft_first_word(&end_word, &start_word, s, c, &words);
+			ft_first_word(&end_word, c, s, &words);
 		else if (*s == c && *(s + 1) != c && *(s + 1) != '\0')
 		{
 			s++;
 			end_word = ft_strchr(s, (int)c);
 			if (end_word)
-			{	
+			{
 				start_word = (char *)s;
 				*words = ft_substr(s, 0, (size_t)(end_word - start_word));
 				words++;
@@ -82,18 +82,18 @@ char	**ft_split(char const *s, char c)
 {
 	char const	*init_pos;
 	char		**words;
-	int		cont;
+	int			cont;
 
 	while (*s == c)
 		s++;
 	init_pos = s;
-	cont = ft_count_words(s, init_pos, c);	
+	cont = ft_count_words(s, init_pos, c);
 	words = malloc((cont + 1) * sizeof(char *));
 	s = init_pos;
 	ft_separate_words(init_pos, s, words, c);
 	return (words);
 }
-
+/*
 int	main(void)
 {
 	char **p;
@@ -105,4 +105,4 @@ int	main(void)
 		p++;
 	}
 	return (0);
-}
+}*/
