0873d613a77aaca1750bd9c5aa6a053f5742fea8
Marcos Jabalquinto Ibanez <mjabalqu@c3r5s5.42malaga.com>
2025-11-24 16:21:51 +0100

    lstadd_back

--- circle_0/Libft/ft_lstadd_back.c
+++ circle_0/Libft/ft_lstadd_back.c
@@ -1,1 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjabalqu <mjabalqu@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 14:47:31 by mjabalqu          #+#    #+#             */
+/*   Updated: 2025/11/24 16:20:59 by mjabalqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
 
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (*lst != NULL)
+		ft_lstlast(*lst) -> next = new;
+	else 
+		*lst = new;
+}	
