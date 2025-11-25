e4998169af4c5a1e47922e601c6c856d0e555007
Marcos Jabalquinto Ibanez <mjabalqu@c3r5s2.42malaga.com>
2025-11-25 10:50:12 +0100

    lstclear

--- circle_0/Libft/ft_lstclear.c
+++ circle_0/Libft/ft_lstclear.c
@@ -1,1 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjabalqu <mjabalqu@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 09:51:55 by mjabalqu          #+#    #+#             */
+/*   Updated: 2025/11/25 10:49:34 by mjabalqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
 
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	if (*lst == NULL)
+		return ;
+	temp = (*lst) -> next;
+	while (*lst != NULL)
+	{
+		del((*lst) -> content);
+		temp = (*lst) -> next;
+		free(*lst);
+		*lst = temp;
+	}
+	*lst = NULL;
+}
