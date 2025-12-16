4906e4b5c85fb840cfcf5e6a02b8ff1abd77a473
Marcos <mbjabalquinto@gmail.com>
2025-12-16 15:15:22 +0100

    update all from home

--- circle_1/get_next_line/get_next_line.c
+++ circle_1/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mjabalqu <mjabalqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 10:36:23 by marcos            #+#    #+#             */
-/*   Updated: 2025/12/16 13:04:30 by mjabalqu         ###   ########.fr       */
+/*   Updated: 2025/12/16 15:14:40 by mjabalqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,21 +33,24 @@ static char	*manage_backup(char **backup)
 static char	*extract_line(char **buffer, char *pos, char **backup)
 {
 	char	*final_string;
+	char	*temp;
 
 	final_string = ft_substr(*backup, 0, (size_t)(pos - *backup + 1));
 	if (!final_string)
-		return (NULL);
+		return (free(*buffer), NULL);
+	
+	temp = ft_substr(*backup, (unsigned int)(pos - *backup + 1),
+			ft_strlen(pos));
+	if (!temp)
+	{
+		free(final_string);
+		free(*buffer);
+		return (NULL);	
+	}
+	free(*backup);
+	*backup = temp;
 	free (*buffer);
 	*buffer = NULL;
-	*buffer = ft_substr(*backup, (unsigned int)(pos - *backup + 1),
-			ft_strlen(pos));
-	if (*buffer)
-	{
-		free(*backup);
-		*backup = *buffer;
-	}
-	else
-		return (NULL);
 	return (final_string);
 }
 
@@ -82,23 +85,29 @@ char	*get_next_line(int fd)
 	char			*pos;
 	int				bytes_read;
 
-	pos = NULL;
 	final_string = NULL;
-	buffer = NULL;
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer || fd < 0 || BUFFER_SIZE <= 0)
 		return (free(buffer), NULL);
-	bytes_read = read_file(fd, &buffer, &backup[fd]);
-	while (pos == NULL && bytes_read > 0)
+	bytes_read = 1;
+	while (bytes_read > 0) 
 	{
-		pos = ft_strchr(backup[fd], '\n');
+		if (backup[fd] != NULL)
+			pos = ft_strchr(backup[fd], '\n');
 		if (pos != NULL)
-			final_string = extract_line(&buffer, pos, &backup[fd]);
+			return (final_string = extract_line(&buffer, pos, &backup[fd]));
 		else
 			bytes_read = read_file(fd, &buffer, &backup[fd]);
 	}
-	if (bytes_read == -1 || !(backup[fd]))
+	if (bytes_read == -1)
+	{	
+		if (backup[fd] != NULL)
+		{
+			free(backup[fd]);
+			backup[fd] = NULL;
+		}
 		return (NULL);
+	}
 	if (pos == NULL && bytes_read == 0 && final_string == NULL
 		&& backup[fd] != NULL)
 		final_string = manage_backup(&backup[fd]);
