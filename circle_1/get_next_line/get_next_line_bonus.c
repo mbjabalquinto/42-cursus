9af3c009d4948a24c5b540845e82330a00db1ec8
mbjabalquinto <124250038+mbjabalquinto@users.noreply.github.com>
2025-12-18 15:37:54 +0100

    Change header include to get_next_line_bonus.h

--- circle_1/get_next_line/get_next_line_bonus.c
+++ circle_1/get_next_line/get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static char	*manage_backup(char **backup)
 {
