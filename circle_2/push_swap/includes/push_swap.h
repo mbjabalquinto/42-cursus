aeef9fffa463bbe73ef55ce495d327dab247d78d
Marcos <mbjabalquinto@gmail.com>
2026-01-29 19:39:04 +0100

    update push swap

--- circle_2/push_swap/includes/push_swap.h
+++ circle_2/push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mjabalqu <mjabalqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 11:42:53 by mjabalqu          #+#    #+#             */
-/*   Updated: 2026/01/29 09:55:40 by mjabalqu         ###   ########.fr       */
+/*   Updated: 2026/01/29 19:31:31 by mjabalqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,28 +33,32 @@ typedef struct s_stack_node
 	struct s_stack_node	*prev; // pointer to the prev node.
 }	t_stack_node;
 
-/* --- parse_and_node_utils.c --- */
+long			ft_atol(const char *str);
+char			**ft_split(char const *s, char c);
 t_stack_node	*find_last_node(t_stack_node *head);
 int				check_nbr(t_stack_node *stack_a, int nbr);
 void			free_split(char **num);
 int				add_node(t_stack_node **stack_a, int nbr);
 int				check_errors(char *num);
-/* --- push_swap.c --- */
+void			prep_for_push(t_stack_node **stack, t_stack_node *node, char stack_name);
+t_stack_node	*return_cheapest(t_stack_node *stack);
+void			move_cheapest(t_stack_node **stack_a, t_stack_node **stack_b);
 int				init_stack(t_stack_node **stack_a, char *arg);
 int				push_swap(t_stack_node **stack_a, t_stack_node **stack_b, char *arg);
-/* --- stack_utils.c --- */
-int				stack_sorted(t_stack_node *stack_a);
-int				sort_stacks(t_stack_node **stack_a, t_stack_node **stack_b);
-void			set_current_position(t_stack_node *stack);
-int				stack_len(t_stack_node *stack);
-void			free_stack(t_stack_node **stack);
-/* --- set_nodes.c --- */
+void			min_on_top(t_stack_node **stack_a);
+t_stack_node	*find_node_min(t_stack_node *stack_a);
+void			set_target_b(t_stack_node **stack_a, t_stack_node **stack_b);
+void			sort_three(t_stack_node **stack_a);
 void			set_cheapest(t_stack_node **stack_a);
 void			cost_analysis(t_stack_node **stack_a, t_stack_node **stack_b);
 t_stack_node	*find_node_max(t_stack_node *stack_b);
 void			set_target(t_stack_node **stack_a, t_stack_node **stack_b);
 void			set_nodes(t_stack_node **stack_a, t_stack_node **stack_b);
-/* --- Operations (swap*.c) --- */
+int				stack_sorted(t_stack_node *stack_a);
+int				sort_stacks(t_stack_node **stack_a, t_stack_node **stack_b);
+void			set_current_position(t_stack_node *stack);
+int				stack_len(t_stack_node *stack);
+void			free_stack(t_stack_node **stack);
 void			pa(t_stack_node **stack_a, t_stack_node **stack_b);
 void			pb(t_stack_node **stack_b, t_stack_node **stack_a);
 void			sa(t_stack_node **stack_a);
@@ -66,7 +70,6 @@ void			rr(t_stack_node **stack_a, t_stack_node **stack_b);
 void			rra(t_stack_node **stack_a);
 void			rrb(t_stack_node **stack_b);
 void			rrr(t_stack_node **stack_a, t_stack_node **stack_b);
-/* --- operations engine (swap.c) --- */
 void			swap(t_stack_node **stack);
 void			push(t_stack_node **target, t_stack_node **source);
 void			rotate(t_stack_node **stack);
