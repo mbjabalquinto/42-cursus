57a452e6265be83c14c2db3d8e472a17b5ce6c3b
Marcos <mbjabalquinto@gmail.com>
2026-03-04 13:10:58 +0100

    Python00 ready to push

--- circle_2/Python00/ex5/ft_count_harvest_recursive.py
+++ circle_2/Python00/ex5/ft_count_harvest_recursive.py
@@ -1,10 +1,3 @@
-def ft_count_harvest_iterative():
-    days = int(input("Days until harvest: "))
-    for i in range(1, days + 1):
-        print("Day", i)
-    print("Harvest time!")
-
-
 def ft_recursive(days):
     if (days > 1):
         ft_recursive(days - 1)
--- circle_2/Python00/ex7/ft_seed_inventory.py
+++ circle_2/Python00/ex7/ft_seed_inventory.py
@@ -1,2 +1,12 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
-
+    if unit == "packets":
+        print(f"{seed_type.capitalize()} seeds: {quantity} {unit} available")
+    elif unit == "grams":
+        print(f"{seed_type.capitalize()} seeds: {quantity} {unit} total")
+    elif unit == "area":
+        print(
+            f"{seed_type.capitalize()} seeds: "
+            f"covers {quantity} squarie meters"
+        )
+    else:
+        print("Unknown unit type")
