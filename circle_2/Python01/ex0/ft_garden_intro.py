eb20f6c69e77d302a470aa1a35e0034b17b2728c
Marcos <mbjabalquinto@gmail.com>
2026-03-04 17:03:32 +0100

    python01

---
+++ circle_2/Python01/ex0/ft_garden_intro.py
@@ -0,0 +1,14 @@
+def ft_garden_intro() -> None:
+    name: str = "rose"
+    height: int = 25
+    age: int = 30
+    print(
+            f"=== Welcome to My Garden ==="
+            f"\nPlant: {name.capitalize()}"
+            f"\nHeight: {height}cm"
+            f"\nAge: {age} days"
+            f"\n=== End of Program ==="
+         )
+
+if __name__ == "__main__":
+    ft_garden_intro()
