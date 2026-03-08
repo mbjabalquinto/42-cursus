061cadb51a4aaccdcbe4ac3d55c708389db1da96
Marcos <mbjabalquinto@gmail.com>
2026-03-08 14:07:23 +0100

    refactor ex5 python01

--- circle_2/Python01/ex5/ft_plant_types.py
+++ circle_2/Python01/ex5/ft_plant_types.py
@@ -13,6 +13,14 @@ class Flower(Plant):
     def bloom(self) -> str:
         return "is blooming beautifully!"
 
+    def __str__(self) -> str:
+        return (
+                f"{self.name.capitalize()} (Flower): "
+                f"{self.height}cm, "
+                f"{self.age} days, "
+                f"{self.color}"
+        )
+
 
 class Tree(Plant):
     def __init__(
@@ -28,6 +36,14 @@ class Tree(Plant):
     def produce_shade(self) -> str:
         return "provides 78 square meters of shade"
 
+    def __str__(self) -> str:
+        return (
+                f"{self.name.capitalize()} (Tree): "
+                f"{self.height}cm, "
+                f"{self.age} days, "
+                f"{self.trunk_diameter}cm diameter"
+        )
+
 
 class Vegetable(Plant):
     def __init__(
@@ -42,63 +58,37 @@ class Vegetable(Plant):
         self.harvest_season = harvest_season
         self.nutritional_value = nutritional_value
 
+    def __str__(self) -> str:
+        return (
+                f"{self.name.capitalize()} (Vegetable): "
+                f"{self.height}cm, "
+                f"{self.age} days, "
+                f"{self.harvest_season}"
+        )
+
 
 def ft_plant_types() -> None:
     print("=== Garden Plant Types ===\n")
-    flower = Flower("rose", 25, 30, "red color")
-    flower2 = Flower("sunflower", 80, 45, "yellow")
-    tree = Tree("oak", 500, 1825, 50)
-    tree2 = Tree("pine", 400, 1000, 30)
-    vegetable = Vegetable("tomato", 80, 90, "summer harvest", "vitamin C")
-    vegetable2 = Vegetable("carrot", 15, 60, "spring harvest", "vitamin A")
-    print(
-            f"{flower.name.capitalize()} (Flower): "
-            f"{flower.height}cm, "
-            f"{flower.age} days, "
-            f"{flower.color}"
-    )
-    print(f"{flower.name.capitalize()} {flower.bloom()}\n")
-    print(
-            f"{tree.name.capitalize()} (Tree): "
-            f"{tree.height}cm, "
-            f"{tree.age} days, "
-            f"{tree.trunk_diameter}cm diameter"
-    )
-    print(f"{tree.name.capitalize()} {tree.produce_shade()}\n")
-    print(
-            f"{vegetable.name.capitalize()} (Vegetable): "
-            f"{vegetable.height}cm, "
-            f"{vegetable.age} days, "
-            f"{vegetable.harvest_season}"
-    )
-    print(
-            f"{vegetable.name.capitalize()} "
-            f"is rich in {vegetable.nutritional_value}"
-    )
-    print(
-            f"\n{flower2.name.capitalize()} (Flower): "
-            f"{flower2.height}cm, "
-            f"{flower2.age} days, "
-            f"{flower2.color}"
-    )
-    print(f"{flower2.name.capitalize()} {flower2.bloom()}\n")
-    print(
-            f"{tree2.name.capitalize()} (Tree): "
-            f"{tree2.height}cm, "
-            f"{tree2.age} days, "
-            f"{tree2.trunk_diameter}cm diameter"
-    )
-    print(f"{tree2.name.capitalize()} {tree2.produce_shade()}\n")
-    print(
-            f"{vegetable2.name.capitalize()} (Vegetable): "
-            f"{vegetable2.height}cm, "
-            f"{vegetable2.age} days, "
-            f"{vegetable2.harvest_season}"
-    )
-    print(
-            f"{vegetable2.name.capitalize()} "
-            f"is rich in {vegetable2.nutritional_value}"
-    )
+    plants: list[Plant]
+    plants = [
+                Flower("rose", 25, 30, "red color"),
+                Flower("sunflower", 80, 45, "yellow"),
+                Tree("oak", 500, 1825, 50),
+                Tree("pine", 400, 1000, 30),
+                Vegetable("tomato", 80, 90, "summer harvest", "vitamin C"),
+                Vegetable("carrot", 15, 60, "spring harvest", "vitamin A")
+              ]
+    for x in plants:
+        print(f"{x}")
+        if isinstance(x, Flower):
+            print(f"{x.name.capitalize()} {x.bloom()}\n")
+        elif isinstance(x, Tree):
+            print(f"{x.name.capitalize()} {x.produce_shade()}\n")
+        elif isinstance(x, Vegetable):
+            print(
+                    f"{x.name.capitalize()} "
+                    f"is rich in {x.nutritional_value}\n"
+            )
 
 
 if __name__ == "__main__":
