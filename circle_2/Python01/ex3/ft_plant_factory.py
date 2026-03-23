4a0e8c46bbdcd2d87d501fb4d49210abbc7996c4
Marcos <mbjabalquinto@gmail.com>
2026-03-23 09:10:33 +0100

    python01 new subject changes ex3

--- circle_2/Python01/ex3/ft_plant_factory.py
+++ circle_2/Python01/ex3/ft_plant_factory.py
@@ -1,31 +1,26 @@
 class Plant:
-    def __init__(self, name: str, height: int, age: int) -> None:
+    def __init__(self, name: str, height: float, age: int) -> None:
         self.name = name
         self.height = height
         self.p_age = age
 
-    def get_info(self) -> str:
-        return (f"{self.name.capitalize()} "
-                f"({self.height}cm, "
-                f"{self.p_age} days)")
+    def show(self) -> None:
+        print(f"Created: {self.name.capitalize()}: "
+              f"{self.height}cm, "
+              f"{self.p_age} days old")
 
 
 def ft_plant_factory() -> None:
-    seeds = [
-            ("rose", 25, 30),
-            ("oak", 200, 365),
-            ("cactus", 5, 90),
-            ("sunflower", 80, 45),
-            ("fern", 15, 120)
+    plants: list[Plant] = [
+            Plant("rose", 25.0, 30),
+            Plant("oak", 200.0, 365),
+            Plant("cactus", 5.0, 90),
+            Plant("sunflower", 80.0, 45),
+            Plant("fern", 15.0, 120)
             ]
     print("=== Plant Factory Output  ===")
-    plants: list[Plant] = []
-    for name, height, age in seeds:
-        plant = Plant(name, height, age)
-        plants.append(plant)
     for x in plants:
-        print(f"Created: {x.get_info()}")
-    print(f"\nTotal plants created: {len(plants)}")
+        x.show()
 
 
 if __name__ == "__main__":
