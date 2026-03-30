f858e87405ea53edccc0e05fc4b5a32d02d4c572
Marcos Jabalquinto Ibanez <mbjabalquinto@gmail.com>
2026-03-30 12:54:56 +0200

    ex4 python02

---
+++ circle_2/Python02/ex4/ft_finally_block.py
@@ -0,0 +1,46 @@
+class PlantError(Exception):
+    def __init__(self, message: str = "Unknown Plant error") -> None:
+        super().__init__(message)
+
+
+def water_plant(plant_name: str) -> None:
+    if plant_name == plant_name.capitalize():
+        print(f"Watering {plant_name}: [OK]")
+    else:
+        raise PlantError(f"Invalid plant name to water: '{plant_name}'")
+
+
+def test_watering_system() -> None:
+    pass
+
+
+def main() -> None:
+    print("=== Garden Watering System ===")
+    print()
+    print("Testing valid plants...")
+    print("Opening watering system")
+    try:
+        water_plant("tomato".capitalize())
+        water_plant("letucce".capitalize())
+        water_plant("carrots".capitalize())
+    except PlantError as e:
+        print(f"Caught PlantError: {e}")
+    finally:
+        print("Closing watering system")
+    print()
+    print("Testing valid plants...")
+    print("Opening watering system")
+    try:
+        water_plant("tomato".capitalize())
+        water_plant("letucce")
+        water_plant("carrots".capitalize())
+    except PlantError as e:
+        print(f"Caught PlantError: {e}")
+    finally:
+        print("Closing watering system")
+    print()
+    print("Cleanup always happens, even with errors!")
+
+
+if __name__ == "__main__":
+    main()
