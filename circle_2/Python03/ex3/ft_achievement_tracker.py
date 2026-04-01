83571931e0cf87e2ac7574108f15c896b6fe1d07
Marcos <mbjabalquinto@gmail.com>
2026-04-01 12:09:30 +0200

    ex3 python03

--- circle_2/Python03/ex3/ft_achievement_tracker.py
+++ circle_2/Python03/ex3/ft_achievement_tracker.py
@@ -1,12 +1,48 @@
 import random
 
 
-def gen_player_achievements() -> None:
-    pass
+def gen_player_achievements(players: list[str], 
+                            achievements: list[str]) -> list[set[str]]:
+    players_achievements: list[set[str]] = []
+    for _ in players:
+        number: int = random.randint(1, len(achievements))
+        players_achievements += [set(random.sample(achievements, number))]
+    return players_achievements
 
 
 def main() -> None:
-    pass
+    players_achievements: list[set[str]] = []
+    players: list[str] = ["Alice", "Bob", "Charlie", "Dylan"]
+    achievements: list[str] = [
+        "Crafting Genius", "Strategist", "World Savior",
+        "Speed Runner", "Survivor", "Master Explorer",
+        "Treasure Hunter", "Unstoppable", "First Steps",
+        "Collector Supreme", "Untouchable", "Sharp Mind", "Boss Slayer"
+    ]
+    print("=== Achievement Tracker System ===")
+    print()
+    players_achievements = gen_player_achievements(players, achievements)
+    i: int = 0
+    for player in players_achievements:
+        print(f"{players[i]}: {player}")
+        i += 1
+    print()
+    print(f"All distinct achievements: {achievements}")
+    temp: set[str] = players_achievements[0]
+    for player in players_achievements[1:]:
+        temp = temp.intersection(player)
+    print(f"Common achievements: {temp}")
+    for i in range(len(players)):
+        temp = set(players_achievements[i])
+        for j in range(len(players_achievements)):
+            if i != j:
+                temp = temp.difference(players_achievements[j])
+        print(f"Only {players[i]} has: {temp}")
+    i = 0
+    for player in players_achievements:
+        temp = set(achievements) - player
+        print(f"{players[i]} is missing: {temp}")
+        i += 1
 
 
 if __name__ == "__main__":
