d64a53c942beab6b1d923641ceb4703a347064b9
Marcos Jabalquinto Ibanez <mbjabalquinto@gmail.com>
2026-04-16 12:22:24 +0200

    python06

--- circle_2/Python06/alchemy/__init__.py
+++ circle_2/Python06/alchemy/__init__.py
@@ -1,5 +1,6 @@
 from .elements import create_air
 from .potions import strength_potion, healing_potion as heal
+from .transmutation.recipes import lead_to_gold
 
 
-__all__ = ["create_air", "strength_potion", "heal"]
+__all__ = ["create_air", "strength_potion", "heal", "lead_to_gold"]
--- circle_2/Python06/ft_transmutation_1.py
+++ circle_2/Python06/ft_transmutation_1.py
@@ -2,8 +2,8 @@ import alchemy.transmutation as transmutation
 
 
 def main() -> None:
-    print("=== Transmutation 0 ===")
-    print("Using file alchemy/transmutation/recipes.py directly")
+    print("=== Transmutation 1 ===")
+    print("Import transmutation module directly")
     print(f"Testing lead to gold: "
           f"{transmutation.lead_to_gold()}")
 
