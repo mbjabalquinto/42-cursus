f3863041949f379c27b916053f660170e6e056ff
Marcos Jabalquinto Ibanez <mbjabalquinto@gmail.com>
2026-05-14 17:01:50 +0200

    ex2 python10

---
+++ circle_2/Python10/ex2/scope_mysteries.py
@@ -0,0 +1,26 @@
+from typing import Callable
+
+
+def mage_counter() -> Callable:
+    counter_a: int = 0
+    counter_b: int = 0
+    def count() -> int:
+        
+        return
+    return count
+
+def spell_accumulator(initial_power: int) -> Callable:
+
+
+def enchantment_factory(enchantment_type: str) -> Callable:
+
+
+def memory_vault() -> dict[str, Callable]:
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
